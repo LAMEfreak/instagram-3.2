5e0464080cadf244e8e4f417e1d1c1a6c7ab2745
LAMEfreak <leeweirong@outlook.com>
2024-03-03 00:26:10 +0800

    Set up Firebase Realtime Database

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ const DB_MESSAGES_KEY = "messages";
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
     // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(messagesRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
+      console.log(data);
       setMessages((prevState) =>
         // Store message key so we can use it as a key in our list items when rendering messages
         [...prevState, { key: data.key, val: data.val() }]
@@ -22,15 +24,25 @@ function App() {
     });
   }, []);
 
-  const writeData = () => {
+  const writeData = (e) => {
+    e.preventDefault();
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    const timestamp = new Date().toLocaleTimeString();
+    set(newMessageRef, { content: message, timestamp });
+    setMessage("");
   };
 
   // Convert messages in state to message JSX elements to render
   let messageListItems = messages.map((message) => (
-    <li key={message.key}>{message.val}</li>
+    <div key={message.key}>
+      <p>
+        {message.val.content}{" "}
+        <span>
+          <i>{message.val.timestamp}</i>
+        </span>
+      </p>
+    </div>
   ));
 
   return (
@@ -40,8 +52,18 @@ function App() {
       </div>
       <h1>Instagram Bootcamp</h1>
       <div className="card">
-        {/* TODO: Add input field and add text input as messages in Firebase */}
-        <button onClick={writeData}>Send</button>
+        <form onSubmit={writeData}>
+          <label htmlFor="messageInput">Enter message: </label>
+          <input
+            type="text"
+            id="messageInput"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit" disabled={!message}>
+            Send
+          </button>
+        </form>
         <ol>{messageListItems}</ol>
       </div>
     </>
